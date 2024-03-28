458b8b96ae083557ebe18e730422a3acd7bfa5bf
Lorenzo Pasqualini <lorpasqu@gmail.com>
2024-03-28 19:05:14 +0100

    v0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'kibra';
+  users= signal<{id: number, name: string}[]>([])
+  adding = signal(false);
+/*   form = new FormGroup({name: new FormControl('', Validators.required)});
+  name= new FormControl(''); */
+
+  form: FormGroup;
+  name: FormControl;
+
+  constructor() {
+    this.name = new FormControl('');
+    this.form = new FormGroup({
+      name: this.name
+    });
+  }
+
+  ngOnInit() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(this.users.set);
+  }
+
+  save(){
+    this.users.update(old=>
+      [...old, { id: 1, name: this.form.value.name ?? '' }]);    
+  }
 }
